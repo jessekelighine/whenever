fe88b94db61582eeaf9db0451d8fe0f56f613ea4
Jesse C. Chen <jessekelighine@gmail.com>
2025-03-13 20:12:29 -0400

    minor update

--- whenever.sh
+++ whenever.sh
@@ -10,28 +10,22 @@
 # Copyright 2023-2024 Jesse C. Chen                                           #
 ###############################################################################
 
-: ${WHENEVER_INTERVAL:=1}
-: ${WHENEVER_COMMAND:=md5sum}
+: "${WHENEVER_INTERVAL:=1}"
+: "${WHENEVER_COMMAND:=md5sum}"
 
 ### Functions #################################################################
 
 message () {
-	local green; green=$(tput bold && tput setaf 10)
-	local blue; blue=$(tput bold && tput setaf 12)
-	local norm; norm=$(tput sgr0)
-	local usage; usage="${green}usage${norm}"
-	local command; command="${blue}$(basename $0)${norm}"
-	local description; description="${green}description${norm}"
 	cat << EOF
-${usage}: ${command} [file|dir] [command]
-${description}: run [command] whenever [file|dir] is modified.
+usage: $(basename "$0") [file|dir] [command]
+description: run [command] whenever [file|dir] is modified.
 EOF
 }
 
 progress () {
 	local yellow; yellow=$(tput bold && tput setaf 214)
 	local norm; norm=$(tput sgr0)
-	printf "${yellow}== whenever: %d ==================================${norm}\n" $1
+	printf "${yellow}== whenever: %d ==================================${norm}\n" "$1"
 }
 
 calc_value () {
@@ -47,7 +41,7 @@ calc_value () {
 
 count=0
 previous_state="$(calc_value "$1")"
-while : ; do sleep $WHENEVER_INTERVAL
+while : ; do sleep "$WHENEVER_INTERVAL"
 	current_state="$(calc_value "$1")"
 	[[ "$previous_state" != "$current_state" ]] && {
 		${@:2}
